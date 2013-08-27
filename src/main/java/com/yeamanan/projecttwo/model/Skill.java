fff1efcb9e76c6d9867eb13e73f94df8b04a8978
unknown <GGL@CONSULTINGW704.corp.castsoftware.com>
2013-08-27 17:36:53 +0200

    Add skill to survivor

---
+++ src/main/java/com/yeamanan/projecttwo/model/Skill.java
@@ -0,0 +1,49 @@
+package com.yeamanan.projecttwo.model;
+
+/**
+ * Skill enum.
+ *
+ * @author Yeam Anan (<yeamanan|at|gmail|dot|com>)
+ */
+public enum Skill {
+
+    /**
+     * Skill list.
+     */
+    PLUS_ONE_ACTION,
+    PLUS_ONE_DAMAGE,
+    PLUS_ONE_DICE_ROLL_COMBAT,
+    PLUS_ONE_DICE_ROLL_MELEE,
+    PLUS_ONE_DICE_ROLL_RANGED,
+    PLUS_ONE_DIE_COMBAT,
+    PLUS_ONE_DIE_MELEE,
+    PLUS_ONE_DIE_RANGED,
+    PLUS_ONE_FREE_COMBAT_ACTION,
+    PLUS_ONE_FREE_MOVE_ACTION,
+    PLUS_ONE_FREE_SEARCH_ACTION,
+    PLUS_ONE_MAX_RANGE,
+    PLUS_ONE_ZONE_PER_MOVE,
+    ONE_REROLL_PER_TURN,
+    TWO_COCKTAILS_ARE_BETTER_THAN_ONE,
+    TWO_ZONE_PER_MOVE_ACTION,
+    AMBIDEXSTROUS,
+    BORN_LEADER,
+    DESTINY,
+    GUNSLINGER,
+    HOARD,
+    HOLD_YOUR_NOSE,
+    IS_THAT_ALL_YOUVE_GOT,
+    LOCK_IT_DOWN,
+    LOUD,
+    LUCKY,
+    MATCHING_SET,
+    MEDIC,
+    NINJA,
+    SLIPPERY,
+    SNIPER,
+    STARTS_WITH_A,
+    SWORDMASTER,
+    TOUGH,
+    TRICK_SHOT;
+
+}
