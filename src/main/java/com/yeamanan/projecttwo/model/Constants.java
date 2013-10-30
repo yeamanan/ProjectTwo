d8e5db692a74898310d5716bcc420e2f721daaac
yeamanan <yeamanan@gmail.com>
2013-10-30 02:00:00 +0100

    Review of Spawn cards + Add of zombie dogz

--- src/main/java/com/yeamanan/projecttwo/model/Constants.java
+++ src/main/java/com/yeamanan/projecttwo/model/Constants.java
@@ -13,12 +13,6 @@ public final class Constants {
     private Constants() { }
 
     /**
-     * Axe's short codes.
-     */
-    public static final String NORTH_SHORT_CODE = "N", EAST_SHORT_CODE = "E",
-            SOUTH_SHORT_CODE = "S", WEST_SHORT_CODE = "W";
-
-    /**
      * State's short codes.
      */
     public static final String OUTDOOR_SHORT_CODE = "0",
@@ -34,18 +28,18 @@ public final class Constants {
      * Default number of action for each character.
      */
     public static final int CHARACTER_ACTION = 0, SURVIVOR_ACTION = 3,
-            ZOMBIE_ACTION = 1, RUNNER_ACTION = 2;
+            ZOMBIE_ACTION = 1, RUNNER_ACTION = 2, ZOMBIE_DOG_ACTION = 3;
 
     /**
      * Damage to kill each type of zombie.
      */
-    public static final int WALKER_DAMAGE = 1, FATTY_DAMAGE = 1,
-            ABOMINATION_DAMAGE = 5, RUNNER_DAMAGE = 1;
+    public static final int WALKER_DAMAGE = 1, FATTY_DAMAGE = 2,
+            ABOMINATION_DAMAGE = 5, RUNNER_DAMAGE = 1, ZOMBIE_DOG_DAMAGE = 1;
 
     /**
      * Experience won when killing a zombie.
      */
     public static final int WALKER_XP = 1, FATTY_XP = 1, ABOMINATION_XP = 5,
-            RUNNER_XP = 1;
+            RUNNER_XP = 1, ZOMBIE_DOG_XP = 1;
 
 }
---
+++ src/main/java/com/yeamanan/projecttwo/model/character/ZombieDog.java
@@ -0,0 +1,20 @@
+package com.yeamanan.projecttwo.model.character;
+
+/**
+ * Abomination class.
+ *
+ * @author Yeam Anan (<yeamanan|at|gmail|dot|com>)
+ */
+public class ZombieDog extends Zombie {
+
+    /**
+     * toString() method.
+     *
+     * @return the walker in string format
+     */
+    @Override
+    public final String toString() {
+        return "Abomination{" + "nbAction=" + this.getNbAction() + '}';
+    }
+
+}
